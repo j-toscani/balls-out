c7b681e4b7f3cce168ae9e538a8b6130d71770e1
Julian Toscani <julian_toscani@gmx.de>
2020-10-26 21:14:35 +0100

    add basic styling, add canvas Element

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    Hello World!
+  <div class="home__container">
+    <canvas width="800" height="600" ref="game_canvas" />
+    <div class="home__buttons-container">
+      <button>small</button>
+      <button>medium</button>
+      <button>big</button>
+    </div>
   </div>
 </template>
 
@@ -9,5 +14,45 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "Home",
+  data(): {
+    canvas: undefined | HTMLCanvasElement;
+  } {
+    return {
+      canvas: undefined,
+    };
+  },
+  mounted() {
+    this.canvas = this.$refs.game_canvas as HTMLCanvasElement;
+  },
 });
 </script>
+
+<style scoped>
+canvas {
+  background: lightgray;
+}
+
+.home__container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-flow: column;
+
+  padding: 20px;
+  height: 100%;
+  width: 100%;
+}
+
+.home__buttons-container {
+  margin: 20px;
+  padding: 20px;
+}
+
+button {
+  text-transform: uppercase;
+  padding: 10px 20px;
+  margin: 0px 20px;
+
+  width: 100px;
+}
+</style>
