6dbe4dbd937d22b2958faf651248a4d6fc51db7b
Julian Toscani <julian_toscani@gmx.de>
2020-11-15 13:19:40 +0100

    add Room component for ready state

---
+++ src/components/Room.vue
@@ -0,0 +1,79 @@
+<template>
+  <section class="room__container-ready">
+    <button class="ready__button" @click="toggleReady">
+      I am Ready
+    </button>
+    <div class="ready__display-container">
+      <span
+        class="ready__display ready__display-me"
+        :class="{
+          ready,
+        }"
+      >
+        You
+      </span>
+      <span class="ready__display ready__display-other"> Opponent</span>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { Room } from "../../types";
+import { defineComponent, PropType } from "vue";
+
+export default defineComponent({
+  name: "Room",
+  data() {
+    return {
+      ready: false,
+      opponentReady: false,
+    };
+  },
+  methods: {
+    toggleReady() {
+      this.ready = !this.ready;
+      console.log("Room Entered");
+    },
+  },
+});
+</script>
+
+<style scoped>
+.ready__display-container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+  margin: 1rem 0;
+}
+
+.room__container-ready {
+  margin: auto;
+  width: 50%;
+  min-width: 300px;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.ready__display {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+
+  color: rgba(var(--text-inverted));
+  background: rgba(var(--warning-100));
+
+  transition: background var(--timing-2) ease-out;
+}
+
+.ready__display.ready {
+  background: rgba(var(--success-100));
+}
+</style>
